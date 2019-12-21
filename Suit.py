189545a71d61465e5a746964012b190aef2523f0
NaughtyPhoton <R3ndertim3>
2019-12-21 15:32:40 -0800

    Testing CardFactory design

--- Suit.py
+++ Suit.py
@@ -47,18 +47,45 @@ class Suit(str, Enum):
 #         raise Exception("Design Failure")
 
 
-def card4(rank: int, suit: Suit) -> Card:
-    class_ = {
-        1: AceCard,
-        11: FaceCard,
-        12: FaceCard,
-        13: FaceCard
-    }.get(rank, Card)
-    return class_(str(rank), suit)
+# def card4(rank: int, suit: Suit) -> Card:
+#     class_ = {
+#         1: AceCard,
+#         11: FaceCard,
+#         12: FaceCard,
+#         13: FaceCard
+#     }.get(rank, Card)
+#     return class_(str(rank), suit)
+
+def card7(rank: int, suit: Suit) -> Card:
+    class_rank = {
+        1: lambda suit: AceCard("A", suit),
+        11: lambda suit: FaceCard("J", suit),
+        12: lambda suit: FaceCard("Q", suit),
+        13: lambda suit: FaceCard("K", suit),
+    }.get(rank, lambda suit: Card(str(rank), suit))
+    return class_rank(suit)
+
+
+class CardFactory:
+
+    def rank(self, rank: str) -> "CardFactory":
+        self.class_, self.rank_str = {
+            1: (AceCard, "A"),
+            11: (FaceCard, "J"),
+            12: (FaceCard, "Q"),
+            13: (FaceCard, "K")
+        }.get(rank, (Card, str(rank)))
+        return self
+
+    def suit(self, suit: Suit) -> Card:
+        return self.class_(self.rank_str, suit)
 
 
 if __name__ == '__main__':
-    deck = [card4(rank, suit)
+    deck = [card7(rank, suit)
             for rank in range(1, 14)
             for suit in list(Suit)]
+
+    cardFactory = CardFactory()
+    cardFactory = [cardFactory.rank(r + 1).suit(s) for r in range(13) for s in Suit]
     print()
