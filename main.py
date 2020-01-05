32ab301c4f27af7a0563071b21975039e2455302
NaughtyPhoton <R3ndertim3>
2020-01-05 11:11:00 -0800

    Added a more complex initializer for the Hand object so that it can be frozen or split.

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ Testing Suit and Card classes.
 """
 from Deck import Deck3 as Deck
 from GameStrategy import GameStrategy
-from Hand import Hand
+from Hand import Hand4 as Hand
 
 if __name__ == '__main__':
     deck = Deck(5)
     hand = Hand(deck.pop(), deck.pop(), deck.pop())
+    frozen_hand = Hand(hand)
+    split_1 = Hand(hand, deck.pop(), split=0)
+    split_2 = Hand(hand, deck.pop(), split=1)
+
+    print(split_1)
+    print(split_2)
+
     dumb = GameStrategy()
-    print('')
+
+    print('c')
--- src/Hand.py
+++ src/Hand.py
@@ -2,7 +2,7 @@
 """
 Blackjack Hand class.
 """
-from typing import List
+from typing import List, Union, Optional, cast, overload
 
 from Card import Card
 
@@ -25,3 +25,55 @@ class Hand:
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__} {self.dealer_card} {self.cards}"
+
+
+class Hand4:
+    """
+    Uses a complex __init__() method with multiple strategies for initialization.
+    """
+
+    @overload
+    def __init__(self, arg1: "Hand4") -> None:
+        ...
+
+    @overload
+    def __init__(self, arg1: "Hand4", arg2: Card, *, split: int) -> None:
+        ...
+
+    @overload
+    def __init__(self, arg1: Card, arg2: Card, arg3: Card) -> None:
+        ...
+
+    def __init__(
+            self,
+            arg1: Union["Hand4", Card],
+            arg2: Optional[Card] = None,
+            arg3: Optional[Card] = None,
+            split: Optional[int] = None,
+    ) -> None:
+        self.dealer_card: Card
+        self.cards: List[Card]
+
+        if isinstance(arg1, Hand4):
+            # Clone an existing hand
+            self.dealer_card = arg1.dealer_card
+            self.cards = arg1.cards
+
+        elif isinstance(arg1, Hand4) and isinstance(arg2, Card) and split is not None:
+            # Split an existing hand
+            self.dealer_card = arg1.dealer_card
+            self.cards = [arg1.cards[split], arg2]
+
+        elif isinstance(arg1, Card) and isinstance(arg2, Card) and isinstance(arg3, Card):
+            # Build a fresh, new hand from three cards
+            self.dealer_card = arg1
+            self.cards = [arg2, arg3]
+
+        else:
+            raise TypeError("Invalid constructor {arg1!r} {arg2!r} {arg3!r}")
+
+    def __str__(self) -> str:
+        return ", ".join(map(str, self.cards))
+
+    def __repr__(self):
+        return f"{self.__class__.__name__} ({self.dealer_card!r}, *{self.cards})"
